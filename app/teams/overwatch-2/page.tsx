89db4c52ae45618660568d9664ecd71e8fe87e97
Nick Stewart <nicktstewart@gmail.com>
2024-09-12 04:29:06 -0400

    [Update] Overwatch team

--- app/teams/overwatch-2/page.tsx
+++ app/teams/overwatch-2/page.tsx
@@ -15,10 +15,20 @@ export default function OverwatchPage() {
         </CardHeader>
         <CardContent>
           <p className="mb-4">
-            Our Overwatch team competes in the Overwatch Collegiate Homecoming
-            during both the Fall and Winter semesters, as well as other
-            tournaments.
+            Our Overwatch 2 team represents McGill in the Overwatch Collegiate
+            Homecoming, a tournament that spans both the Fall and Winter
+            semester, for prizes up to $20,000.
           </p>
+          <h3 className="font-semibold mb-2">Competitive Schedule</h3>
+          <ul className="list-disc pl-5">
+            <li>
+              <strong>Overwatch Collegiate Homecoming:</strong> Fall tournament
+              starting September 28th hosted by Activision-Blizzard and Radiant.
+            </li>
+            <li>
+              Teams are free to join additional tournaments at their discretion.
+            </li>
+          </ul>
         </CardContent>
       </Card>
 
@@ -27,21 +37,33 @@ export default function OverwatchPage() {
           <CardTitle className="text-xl">Recruitment</CardTitle>
         </CardHeader>
         <CardContent>
-          <p className="mb-4">
-            Players of any skill level are welcome! We expect you to be
-            available to scrim other collegiate teams 2-4 times a week. Click
-            the button below to fill out our interest form!
-          </p>
-          <Link
-            href="https://docs.google.com/forms/d/e/1FAIpQLSd82mT8H5Hs5WP5OiJ-v_4yfZLQVy1OiuhhdG22C7J1sjpNUg/viewform?ts=66ccc15a&edit_requested=true"
-            target="_blank"
-          >
+          <h3 className="font-semibold mb-2">Who are we looking for?</h3>
+          <ul className="list-disc pl-5 mb-4">
+            <li>
+              Players of any skill level who are interested in competitive
+              Overwatch!
+            </li>
+            <li>
+              We expect you to be able to scrim other teams 2-4 times per week.
+            </li>
+          </ul>
+          <Link href="https://forms.gle/2WmcpHag9gK6Y4ub8" target="_blank">
             <Button className="bg-mcgill hover:bg-mcgill-vibrant">
               Join Our Team
             </Button>
           </Link>
         </CardContent>
       </Card>
+
+      <h2 className="text-2xl font-semibold mb-4">Results</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-3 mb-8">
+        <ResultCard
+          tournamentName="Overwatch Collegiate Homecoming 2023"
+          logo="/overwatch-2/homecoming.png"
+          result="Top 20% out of 340 teams"
+        />
+        {/* Add more ResultCards as needed */}
+      </div>
     </div>
   );
 }
